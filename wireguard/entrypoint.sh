37977200403e050ef8660426013cd642a924d9af
Michael Sherman <shermanm@uchicago.edu>
2022-03-17 15:22:18 -0500

    return to normalcy

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,9 +2,15 @@
 
 modprobe udp_tunnel
 modprobe ip6_udp_tunnel
-lsmod | grep -q wireguard || {
-  # If the kernel module wasn't available, we would have built it here
-  insmod /wireguard/wireguard.ko || true
-}
+
+OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
+echo "OS Version is $OS_VERSION"
+
+cd output
+mod_dir="wireguard-linux-compat_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+for each in $mod_dir; do
+	echo Loading module from "$each"
+	insmod "$each/wireguard.ko" || true
+done
 
 exec "$@"
